0ad05faf974e4d1957105286d56e7943c7894854
F4rab1 <issafarabi2022@gmail.com>
2024-07-17 21:22:41 +0500

    Tasks list API

--- backend/task/views.py
+++ backend/task/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Task
+from .serializers import TaskSerializer
+from datetime import datetime
 
-def say_hello(request):
-    return HttpResponse('Hello World!')
-
-# Create your views here.
+@api_view(['GET', 'POST'])
+def task_list(request):
+    if request.method == 'GET':
+        query_set = Task.objects.all()
+        serializer = TaskSerializer(query_set, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = TaskSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.validated_data)
