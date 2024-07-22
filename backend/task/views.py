db3f77e05352eeea0d466d33d70badac0d2b9bf6
F4rab1 <issafarabi2022@gmail.com>
2024-07-23 02:21:07 +0500

    New Task Model and tasks by completion_date

--- backend/task/views.py
+++ backend/task/views.py
@@ -11,8 +11,11 @@ class TaskViewSet(ModelViewSet):
     def get_queryset(self):
         queryset = Task.objects.select_related('category').all()
         category_id = self.request.query_params.get('category_id')
+        completion_date = self.request.query_params.get('completion_date')
         if category_id is not None:
             queryset = queryset.filter(category_id=category_id)
+        elif completion_date is not None:
+            queryset = queryset.filter(completion_date=completion_date)
         return queryset
 
 
