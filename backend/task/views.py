1935d46ada1b4ac7fd7eb0f970fb001904be3060
F4rab1 <issafarabi2022@gmail.com>
2024-07-22 23:05:26 +0500

    ViewSet and DefaultRouter added

--- backend/task/views.py
+++ backend/task/views.py
@@ -1,38 +1,15 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
 from .models import Task, Category
 from .serializers import TaskSerializer, CategorySerilizer
-from datetime import datetime
 
-@api_view(['GET', 'POST'])
-def task_list(request):
-    if request.method == 'GET':
-        query_set = Task.objects.select_related('category').all()
-        serializer = TaskSerializer(query_set, many=True)
-        return Response(serializer.data)
-    elif request.method == 'POST':
-        serializer = TaskSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.validated_data)
-    
 
-@api_view(['GET', 'PUT'])
-def task_detail(request, id):
-    task = Task.objects.get(pk=id)
-    if request.method == 'GET':
-        serializer = TaskSerializer(task)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = TaskSerializer(task, data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-    
+class TaskViewSet(ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
 
-@api_view(['GET', 'PUT'])
-def category_list(request):
-    if request.method == 'GET':
-        queryset = Category.objects.all()
-        serializer = CategorySerilizer(queryset, many=True)
-        return Response(serializer.data)
+
+class CategoryViewSet(ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerilizer
