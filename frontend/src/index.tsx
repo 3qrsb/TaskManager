ef8ea71125a8f6487f14daf0224722f1ae5a5a29
sselluos <106395084+3qrsb@users.noreply.github.com>
2024-07-23 20:31:02 +0500

    che za (#17)

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "semantic-ui-css/semantic.min.css";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import theme from "./styles/theme";
 
-const rootElement = document.getElementById("root");
-
-if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
-    </React.StrictMode>
-  );
-} else {
-  console.error("Root element not found");
-}
+    </ChakraProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
