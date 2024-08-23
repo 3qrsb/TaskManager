b047cbacf21fc00b435d69b98a23c91666d2f526
Yers <eabuov4@gmail.com>
2024-08-23 23:41:12 +0500

    removing not needed component

--- frontend/src/layout/Header.tsx
+++ frontend/src/layout/Header.tsx
@@ -1,18 +1,7 @@
-import {
-  Box,
-  Flex,
-  Image,
-  IconButton,
-  Button,
-  useDisclosure,
-  useColorModeValue,
-} from "@chakra-ui/react";
-import { HamburgerIcon } from "@chakra-ui/icons";
+import { Box, Flex, Image, Button, useColorModeValue } from "@chakra-ui/react";
 import ColorModeSwitch from "../components/ColorModeSwitch";
-import MobileMenu from "./MobileMenu";
 
 const Header = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
   const headerBg = useColorModeValue(
     "headerFooterBg.light",
     "headerFooterBg.dark"
@@ -23,15 +12,7 @@ const Header = () => {
     <Box as="header" p={{ base: 3, md: 4 }} bg={headerBg}>
       <Flex justify="space-between" align="center">
         <Image src="logo.webp" alt="Logo" boxSize="45px" />
-        <IconButton
-          icon={<HamburgerIcon />}
-          display={{ base: "block", md: "none" }}
-          onClick={onOpen}
-          aria-label="Open Menu"
-          bg={buttonHoverBg}
-          _hover={{ bg: buttonHoverBg }}
-        />
-        <Flex display={{ base: "none", md: "flex" }} align="center" gap={4}>
+        <Flex display={{ base: "flex", md: "flex" }} align="center" gap={4}>
           <Button variant="ghost" _hover={{ bg: buttonHoverBg }}>
             Home
           </Button>
@@ -44,7 +25,6 @@ const Header = () => {
           <ColorModeSwitch />
         </Flex>
       </Flex>
-      <MobileMenu isOpen={isOpen} onClose={onClose} />
     </Box>
   );
 };
